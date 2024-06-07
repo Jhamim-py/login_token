c230f79ad0f10dce38654999fafca9171913387c
Jhamim-py <141651527+Jhamim-py@users.noreply.github.com>
2024-06-07 11:25:51 -0300

    CORS

--- loginjwt/app.js
+++ loginjwt/app.js
@@ -13,6 +13,19 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+//CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
 
 //Models
 const User = require('./models/User');
